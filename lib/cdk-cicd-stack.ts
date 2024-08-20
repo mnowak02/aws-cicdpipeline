0226aba1d62c199551346924d48c310874c71a06
Mariusz N <mnowak02@wp.pl>
2024-08-20 02:39:08 +0200

    stage added and lambda

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,13 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'AwesomePipeline', {
+    const pipeline = new CodePipeline(this, 'AwesomePipeline', {
       pipelineName: 'AwesomePipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('mnowak02/aws-cicdpipeline', 'cicd-practice'),
@@ -20,7 +21,10 @@ export class CdkCicdStack extends cdk.Stack {
         ],
       })
 
-    })
+    });
+    const testStage = pipeline.addStage(new PipelineStage(this, 'PipelineTestStage',{
+      stageName:'test1'
+    }))
 
 
   }
